42d26e48339951158d095fd5576b26b49296555a
Filip Vidlák <vidlakf.03@spst.eu>
2021-12-20 23:36:17 +0100

    Generate, reserve, buy

--- CineKometa/Form1.Designer.cs
+++ CineKometa/Form1.Designer.cs
@@ -29,13 +29,51 @@ namespace CineKometa
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.koupeni = new System.Windows.Forms.RadioButton();
+            this.rezervace = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // koupeni
+            // 
+            this.koupeni.AutoSize = true;
+            this.koupeni.Location = new System.Drawing.Point(643, 379);
+            this.koupeni.Name = "koupeni";
+            this.koupeni.Size = new System.Drawing.Size(67, 21);
+            this.koupeni.TabIndex = 0;
+            this.koupeni.TabStop = true;
+            this.koupeni.Text = "koupit";
+            this.koupeni.UseVisualStyleBackColor = true;
+            // 
+            // rezervace
+            // 
+            this.rezervace.AutoSize = true;
+            this.rezervace.Location = new System.Drawing.Point(643, 341);
+            this.rezervace.Name = "rezervace";
+            this.rezervace.Size = new System.Drawing.Size(92, 21);
+            this.rezervace.TabIndex = 1;
+            this.rezervace.TabStop = true;
+            this.rezervace.Text = "rezervace";
+            this.rezervace.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rezervace);
+            this.Controls.Add(this.koupeni);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton koupeni;
+        private System.Windows.Forms.RadioButton rezervace;
     }
 }
 
--- CineKometa/Form1.cs
+++ CineKometa/Form1.cs
@@ -16,5 +16,82 @@ namespace CineKometa
         {
             InitializeComponent();
         }
+        PictureBox[,] Seats = new PictureBox[6,15];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    string path = "../../Pictures/empty.png";
+                    if (j > 0 && Seats[i, j - 1].ImageLocation == "../../Pictures/leftempty.png") { path = "../../Pictures/rightempty.png"; }
+                    else if(rnd.Next(0,5) == 1 && j < 14) { path = "../../Pictures/leftempty.png"; }
+                    Seats[i, j] = new PictureBox
+                    {
+                        Name = "PictureBox:" + i.ToString() + ":" + j.ToString(),
+                        Size = new Size(30, 30),
+                        Location = new Point(33 * j + 50, 33 * i + 20),
+                        ImageLocation = @path,
+                        SizeMode = PictureBoxSizeMode.StretchImage
+                    };
+                    Seats[i, j].Click += new EventHandler(ChangeSeat);
+                    this.Controls.Add(Seats[i, j]);
+                }
+            }
+        }
+
+        private void ChangeSeat(object sender, EventArgs e)
+        {
+            if ((sender as PictureBox).ImageLocation == "../../Pictures/empty.png") {
+                if (rezervace.Checked == true) {
+                    (sender as PictureBox).ImageLocation = @"../../Pictures/..";
+                    MessageBox.Show("Rezervoval sis místo Řada: " + (sender as PictureBox).Name.Split(':')[1] + "  Sedadlo: " + (sender as PictureBox).Name.Split(':')[2]);
+
+
+                }
+    
+                else {
+                    (sender as PictureBox).ImageLocation = @"../../Pictures/..";
+                    MessageBox.Show("Koupil sis místo Řada: " + (sender as PictureBox).Name.Split(':')[1] + "  Sedadlo: " + (sender as PictureBox).Name.Split(':')[2]);
+                }
+
+            }
+            else if ((sender as PictureBox).ImageLocation == "../../Pictures/leftempty.png")
+            {
+                if (rezervace.Checked == true)
+                {
+                    (sender as PictureBox).ImageLocation = @"../../Pictures/leftreserve.png";
+                    Seats[Int32.Parse((sender as PictureBox).Name.Split(':')[1]), Int32.Parse((sender as PictureBox).Name.Split(':')[2]) + 1].ImageLocation = @"../../Pictures/rightreserve.png";
+                    MessageBox.Show("Rezervoval sis místo Řada: " + (sender as PictureBox).Name.Split(':')[1] + "  Sedadlo: " + (sender as PictureBox).Name.Split(':')[2]);
+                }
+                else
+                {
+                    (sender as PictureBox).ImageLocation = @"../../Pictures/leftbuy.png";
+                    Seats[Int32.Parse((sender as PictureBox).Name.Split(':')[1]), Int32.Parse((sender as PictureBox).Name.Split(':')[2]) + 1].ImageLocation = @"../../Pictures/leftbuy.png";
+                    MessageBox.Show("Koupil sis místo Řada: " + (sender as PictureBox).Name.Split(':')[1] + "  Sedadlo: " + (sender as PictureBox).Name.Split(':')[2]);
+                }
+            }
+            else if ((sender as PictureBox).ImageLocation == "../../Pictures/rightempty.png")
+            {
+                if (rezervace.Checked == true)
+                {
+                    (sender as PictureBox).ImageLocation = @"../../Pictures/rightreserve.png";
+                    Seats[Int32.Parse((sender as PictureBox).Name.Split(':')[1]), Int32.Parse((sender as PictureBox).Name.Split(':')[2]) - 1].ImageLocation = @"../../Pictures/leftreserve.png";
+                    MessageBox.Show("Rezervoval sis místo Řada: " + (sender as PictureBox).Name.Split(':')[1] + "  Sedadlo: " + (sender as PictureBox).Name.Split(':')[2]);
+
+
+                }
+
+                else
+                {
+                    (sender as PictureBox).ImageLocation = @"../../Pictures/rightbuy.png";
+                    Seats[Int32.Parse((sender as PictureBox).Name.Split(':')[1]), Int32.Parse((sender as PictureBox).Name.Split(':')[2]) - 1].ImageLocation = @"../../Pictures/leftbuy.png";
+                    MessageBox.Show("Koupil sis místo Řada: " + (sender as PictureBox).Name.Split(':')[1] + "  Sedadlo: " + (sender as PictureBox).Name.Split(':')[2]);
+                }
+            }
+        }
     }
+
 }
+
